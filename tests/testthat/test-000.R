294f77c3d5bf2878856c3f5c23aca5266150444b
Jon Harmon <jon.harmon@macmillan.com>
2019-09-10 16:09:35 -0500

    What if we install tensorflow inside the test?

--- tests/testthat/test-000.R
+++ tests/testthat/test-000.R
@@ -3,6 +3,6 @@
 # system(paste(conda, "info --json"))
 
 test_that("TF is working.", {
-  library(tensorflow)
+  tensorflow::install_tensorflow(version = "1.12.0", restart_session = FALSE)
   expect_true(!is.null(tensorflow::tf_version()))
 })
