f73df106ff50007d4d1986c498f50cedec995553
Jon Harmon <jon.harmon@macmillan.com>
2019-09-10 13:43:47 -0500

    Added additional test info and the explicit repo for install_version.

--- tests/testthat/test-000.R
+++ tests/testthat/test-000.R
@@ -1,3 +1,7 @@
+reticulate:::conda_list()
+conda <- reticulate:::conda_binary("auto")
+system(paste(conda, "info --json"))
+
 test_that("TF is working.", {
   expect_true(!is.null(tensorflow::tf_version()))
 })
