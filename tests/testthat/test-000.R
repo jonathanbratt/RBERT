c9bcc1e818daa4447ac679d09dd99a8e170462a8
Jon Harmon <jon.harmon@macmillan.com>
2019-09-10 15:59:53 -0500

    Make sure tensorflow gets libraried, I guess?

--- tests/testthat/test-000.R
+++ tests/testthat/test-000.R
@@ -1,7 +1,8 @@
-reticulate:::conda_list()
-conda <- reticulate:::conda_binary("auto")
-system(paste(conda, "info --json"))
+# reticulate:::conda_list()
+# conda <- reticulate:::conda_binary("auto")
+# system(paste(conda, "info --json"))
 
 test_that("TF is working.", {
+  library(tensorflow)
   expect_true(!is.null(tensorflow::tf_version()))
 })
