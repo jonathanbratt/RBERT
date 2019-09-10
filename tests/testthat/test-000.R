63f6d1a26f20a8b35dd26f1c0b5ef041cf0cc42b
Jon Harmon <jon.harmon@macmillan.com>
2019-09-10 17:11:45 -0500

    Try installing dev version of reticulate.

--- tests/testthat/test-000.R
+++ tests/testthat/test-000.R
@@ -3,19 +3,19 @@
 # system(paste(conda, "info --json"))
 
 test_that("TF is working.", {
-  registry_versions <- reticulate::py_versions_windows()
-  anaconda_registry_versions <- subset(registry_versions,
-                                       registry_versions$type == "Anaconda")
-  print(
-    paste(
-      file.path(anaconda_registry_versions$install_path, "Scripts", "conda.exe"),
-      collapse = " | "
-    )
-  )
+  # registry_versions <- reticulate::py_versions_windows()
+  # anaconda_registry_versions <- subset(registry_versions,
+  #                                      registry_versions$type == "Anaconda")
+  # print(
+  #   paste(
+  #     file.path(anaconda_registry_versions$install_path, "Scripts", "conda.exe"),
+  #     collapse = " | "
+  #   )
+  # )
 
   # condas <- paste(reticulate::conda_list()$name, collapse = " | ")
   # print(condas)
   # expect_identical(condas, "this is not that")
-  reticulate::use_condaenv('r-reticulate')
+  # reticulate::use_condaenv('r-reticulate')
   expect_true(!is.null(tensorflow::tf_version()))
 })
