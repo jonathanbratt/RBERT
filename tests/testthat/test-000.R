f2a5a2d83c1e3234af039e5f27b0cc995de2c86f
Jon Harmon <jon.harmon@macmillan.com>
2019-09-10 17:05:04 -0500

    Drilling into exactly where the bug occurs.

--- tests/testthat/test-000.R
+++ tests/testthat/test-000.R
@@ -3,6 +3,16 @@
 # system(paste(conda, "info --json"))
 
 test_that("TF is working.", {
+  registry_versions <- reticulate::py_versions_windows()
+  anaconda_registry_versions <- subset(registry_versions,
+                                       registry_versions$type == "Anaconda")
+  print(
+    paste(
+      file.path(anaconda_registry_versions$install_path, "Scripts", "conda.exe"),
+      collapse = " | "
+    )
+  )
+
   # condas <- paste(reticulate::conda_list()$name, collapse = " | ")
   # print(condas)
   # expect_identical(condas, "this is not that")
